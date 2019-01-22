90ebc4fc2d9930df29d7f000aa152c16ea9414a9
cyl <761364115@qq.com>
2019-01-22 16:16:48 +0800

    推送初始化，接收器receiver

--- app/src/main/java/com/ying/administrator/masterappdemo/app/MyApplication.java
+++ app/src/main/java/com/ying/administrator/masterappdemo/app/MyApplication.java
@@ -1,6 +1,35 @@
 package com.ying.administrator.masterappdemo.app;
 
 import android.app.Application;
+import android.util.Log;
+
+import com.tencent.android.tpush.XGIOperateCallback;
+import com.tencent.android.tpush.XGPushConfig;
+import com.tencent.android.tpush.XGPushManager;
 
 public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        XGPushConfig.enableDebug(this,true);
+        XGPushConfig.enableOtherPush(getApplicationContext(), true);
+        XGPushConfig.setHuaweiDebug(true);
+        XGPushConfig.setMiPushAppId(getApplicationContext(), "ed915472a691f");
+        XGPushConfig.setMiPushAppKey(getApplicationContext(), "c10d44005770d36ea1822e36748fe9ce");
+        XGPushConfig.setMzPushAppId(this, "ed915472a691f");
+        XGPushConfig.setMzPushAppKey(this, "c10d44005770d36ea1822e36748fe9ce");
+        XGPushManager.registerPush(this, new XGIOperateCallback() {
+            @Override
+            public void onSuccess(Object data, int flag) {
+//token在设备卸载重装的时候有可能会变
+                Log.d("TPush", "注册成功，设备token为：" + data);
+            }
+            @Override
+            public void onFail(Object data, int errCode, String msg) {
+                Log.d("TPush", "注册失败，错误码：" + errCode + ",错误信息：" + msg);
+            }
+        });
+        XGPushManager.bindAccount(getApplicationContext(), "XINGE");
+        XGPushManager.setTag(this,"XINGE");
+    }
 }
---
+++ app/src/main/java/com/ying/administrator/masterappdemo/receiver/XGPushReceiver.java
@@ -0,0 +1,82 @@
+package com.ying.administrator.masterappdemo.receiver;
+
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
+
+import com.tencent.android.tpush.XGPushBaseReceiver;
+import com.tencent.android.tpush.XGPushClickedResult;
+import com.tencent.android.tpush.XGPushRegisterResult;
+import com.tencent.android.tpush.XGPushShowedResult;
+import com.tencent.android.tpush.XGPushTextMessage;
+
+import java.io.IOException;
+
+public class XGPushReceiver extends XGPushBaseReceiver {
+    @Override
+    public void onRegisterResult(Context context, int i, XGPushRegisterResult xgPushRegisterResult) {
+
+    }
+
+    @Override
+    public void onUnregisterResult(Context context, int i) {
+
+    }
+
+    @Override
+    public void onSetTagResult(Context context, int i, String s) {
+
+    }
+
+    @Override
+    public void onDeleteTagResult(Context context, int i, String s) {
+
+    }
+
+    /**
+     * 接收消息
+     * @param context
+     * @param xgPushTextMessage
+     */
+    @Override
+    public void onTextMessage(Context context, XGPushTextMessage xgPushTextMessage) {
+        xgPushTextMessage.getCustomContent();
+    }
+
+    /**
+     * 通知被点击
+     * @param context
+     * @param xgPushClickedResult
+     */
+    @Override
+    public void onNotifactionClickedResult(Context context, XGPushClickedResult xgPushClickedResult) {
+
+    }
+
+    /**
+     * 接收通知
+     * @param context
+     * @param xgPushShowedResult
+     */
+    @Override
+    public void onNotifactionShowedResult(Context context, XGPushShowedResult xgPushShowedResult) {
+        openAssetMusics(context);
+    }
+    /**
+     * 打开assets下的音乐mp3文件
+     */
+    private void openAssetMusics(Context context) {
+
+        try {
+            //播放 assets/a2.mp3 音乐文件
+            AssetFileDescriptor fd = context.getAssets().openFd("repair_order.mp3");
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDataSource(fd.getFileDescriptor(), fd.getStartOffset(), fd.getLength());
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
