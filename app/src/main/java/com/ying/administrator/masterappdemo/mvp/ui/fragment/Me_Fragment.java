a7ad85bc420d5f8886813a4c19f827c42ce984ce
Administrator <765316930@qq.com>
2019-02-18 13:36:49 +0800

    页面修改

--- app/src/main/java/com/ying/administrator/masterappdemo/mvp/ui/fragment/Me_Fragment.java
+++ app/src/main/java/com/ying/administrator/masterappdemo/mvp/ui/fragment/Me_Fragment.java
@@ -13,14 +13,19 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.ying.administrator.masterappdemo.R;
+import com.ying.administrator.masterappdemo.common.DefineView;
 import com.ying.administrator.masterappdemo.mvp.ui.activity.AboutUsActivity;
 import com.ying.administrator.masterappdemo.mvp.ui.activity.Opinion_Activity;
+import com.ying.administrator.masterappdemo.mvp.ui.activity.Order_Receiving_Activity;
 import com.ying.administrator.masterappdemo.mvp.ui.activity.Personal_Information_Activity;
 import com.ying.administrator.masterappdemo.mvp.ui.activity.SettingActivity;
 import com.ying.administrator.masterappdemo.mvp.ui.activity.Wallet_Activity;
 import com.ying.administrator.masterappdemo.mvp.ui.activity.WithDrawActivity;
-import com.ying.administrator.masterappdemo.common.DefineView;
 import com.ying.administrator.masterappdemo.mvp.ui.fragment.BaseFragment.BaseFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 public class Me_Fragment extends BaseFragment implements DefineView {
     private static final String ARG_SHOW_TEXT = "text";
@@ -32,6 +37,7 @@ public class Me_Fragment extends BaseFragment implements DefineView {
     private LinearLayout ll_me_about_us;
     private LinearLayout ll_me_information;
     private LinearLayout ll_me_opinion;
+
     public Me_Fragment() {
         // Required empty public constructor
     }
@@ -45,6 +51,7 @@ public class Me_Fragment extends BaseFragment implements DefineView {
 
 
     }
+
     public static Me_Fragment newInstance(String param1) {
         Me_Fragment fragment = new Me_Fragment();
         Bundle args = new Bundle();
@@ -57,9 +64,9 @@ public class Me_Fragment extends BaseFragment implements DefineView {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        if (view==null){
+        if (view == null) {
             view = inflater.inflate(R.layout.fragment_me, container, false);
-            Log.d("ying","调用了onCreateView");
+            Log.d("ying", "调用了onCreateView");
             initView();
             initValidata();
             initListener();
@@ -70,12 +77,12 @@ public class Me_Fragment extends BaseFragment implements DefineView {
 
     @Override
     public void initView() {
-        tv_me_withdraw_deposit=view.findViewById(R.id.tv_me_withdraw_deposit);
-        ll_me_mywallet=view.findViewById(R.id.ll_me_mywallet);
-        img_me_setting=view.findViewById(R.id.img_me_setting);
-        ll_me_about_us=view.findViewById(R.id.ll_me_about_us);
-        ll_me_information=view.findViewById(R.id.ll_me_information);
-        ll_me_opinion=view.findViewById(R.id.ll_me_opinion);
+        tv_me_withdraw_deposit = view.findViewById(R.id.tv_me_withdraw_deposit);
+        ll_me_mywallet = view.findViewById(R.id.ll_me_mywallet);
+        img_me_setting = view.findViewById(R.id.img_me_setting);
+        ll_me_about_us = view.findViewById(R.id.ll_me_about_us);
+        ll_me_information = view.findViewById(R.id.ll_me_information);
+        ll_me_opinion = view.findViewById(R.id.ll_me_opinion);
     }
 
     @Override
@@ -91,6 +98,7 @@ public class Me_Fragment extends BaseFragment implements DefineView {
         ll_me_about_us.setOnClickListener(new CustomOnclickListner());
         ll_me_information.setOnClickListener(new CustomOnclickListner());
         ll_me_opinion.setOnClickListener(new CustomOnclickListner());
+
     }
 
     @Override
@@ -98,30 +106,36 @@ public class Me_Fragment extends BaseFragment implements DefineView {
 
     }
 
-    public class CustomOnclickListner implements View.OnClickListener{
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+    }
+
+    public class CustomOnclickListner implements View.OnClickListener {
         @Override
         public void onClick(View v) {
-      switch (v.getId()){
-          case R.id.tv_me_withdraw_deposit: //提现
-              startActivity(new Intent(getActivity(),WithDrawActivity.class));
-              break;
-          case R.id.ll_me_mywallet:  //我的钱包
-              startActivity(new Intent(getActivity(), Wallet_Activity.class));
-              break;
-          case R.id.img_me_setting: //设置
-              startActivity(new Intent(getActivity(), SettingActivity.class));
-              break;
-          case R.id.ll_me_about_us://关于我们
-              startActivity(new Intent(getActivity(), AboutUsActivity.class));
-              break;
-          case R.id.ll_me_information: //个人信息管理
-              startActivity(new Intent(getActivity(), Personal_Information_Activity.class));
-              break;
-          case R.id.ll_me_opinion://意见反馈
-              startActivity(new Intent(getActivity(), Opinion_Activity.class));
-              default:
-                  break;
-      }
+            switch (v.getId()) {
+                case R.id.tv_me_withdraw_deposit: //提现
+                    startActivity(new Intent(getActivity(), WithDrawActivity.class));
+                    break;
+                case R.id.ll_me_mywallet:  //我的钱包
+                    startActivity(new Intent(getActivity(), Wallet_Activity.class));
+                    break;
+                case R.id.img_me_setting: //设置
+                    startActivity(new Intent(getActivity(), SettingActivity.class));
+                    break;
+                case R.id.ll_me_about_us://关于我们
+                    startActivity(new Intent(getActivity(), AboutUsActivity.class));
+                    break;
+                case R.id.ll_me_information: //个人信息管理
+                    startActivity(new Intent(getActivity(), Personal_Information_Activity.class));
+                    break;
+                case R.id.ll_me_opinion://意见反馈
+                    startActivity(new Intent(getActivity(), Opinion_Activity.class));
+                default:
+                    break;
+            }
         }
     }
 }
