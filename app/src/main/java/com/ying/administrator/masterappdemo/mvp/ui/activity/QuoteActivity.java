85957888a758650616ead6222d9cc31828cd016f
danchaofann <765316930@qq.com>
2020-01-13 15:28:04 +0800

    报价界面

---
+++ app/src/main/java/com/ying/administrator/masterappdemo/mvp/ui/activity/QuoteActivity.java
@@ -0,0 +1,100 @@
+package com.ying.administrator.masterappdemo.mvp.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.ying.administrator.masterappdemo.R;
+import com.ying.administrator.masterappdemo.base.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/*报价*/
+public class QuoteActivity extends BaseActivity implements View.OnClickListener{
+    @BindView(R.id.img_actionbar_return)
+    ImageView mImgActionbarReturn;
+    @BindView(R.id.tv_actionbar_return)
+    TextView mTvActionbarReturn;
+    @BindView(R.id.ll_return)
+    LinearLayout mLlReturn;
+    @BindView(R.id.tv_actionbar_title)
+    TextView mTvActionbarTitle;
+    @BindView(R.id.img_actionbar_message)
+    ImageView mImgActionbarMessage;
+    @BindView(R.id.tv_message)
+    TextView mTvMessage;
+    @BindView(R.id.actionbar_layout)
+    RelativeLayout mActionbarLayout;
+    @BindView(R.id.tv_payment_method)
+    TextView mTvPaymentMethod;
+    @BindView(R.id.tv_order_time)
+    TextView mTvOrderTime;
+    @BindView(R.id.tv_work_order_number)
+    TextView mTvWorkOrderNumber;
+    @BindView(R.id.iv_copy)
+    ImageView mIvCopy;
+    @BindView(R.id.tv_type)
+    TextView mTvType;
+    @BindView(R.id.tv_cause_of_issue)
+    TextView mTvCauseOfIssue;
+    @BindView(R.id.tv_number)
+    TextView mTvNumber;
+    @BindView(R.id.tv_classification)
+    TextView mTvClassification;
+    @BindView(R.id.tv_description)
+    TextView mTvDescription;
+    @BindView(R.id.tv_product_type)
+    TextView mTvProductType;
+    @BindView(R.id.tv_name)
+    TextView mTvName;
+    @BindView(R.id.tv_phone)
+    TextView mTvPhone;
+    @BindView(R.id.tv_address)
+    TextView mTvAddress;
+    @BindView(R.id.rv_picture)
+    RecyclerView mRvPicture;
+    @BindView(R.id.et_price)
+    EditText mEtPrice;
+
+    @Override
+    protected int setLayoutId() {
+        return R.layout.activity_quote;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView() {
+        mTvActionbarTitle.setText("报价详情");
+    }
+
+    @Override
+    protected void setListener() {
+        mLlReturn.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.ll_return:
+                finish();
+                break;
+        }
+    }
+}
